e82bc6fe9ed2c2e787cee42c09d56aa278fc532b
Tolloviis <tuukka.heikkila07@gmail.com>
2025-04-24 13:58:46 +0300

    Parempi kulma
    Pitää vielä laittaa niin ettei se vaan ilmesty vasemmalle tai oikealle

--- toukka_kontrollit.gd
+++ toukka_kontrollit.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-@export var lane_offset := 2.0              # Distance between lanes
+@export var lane_offset := 1.0              # Distance between lanes
 @export var total_lanes := 3                # Should be an odd number (so there's a center lane)
 
 var current_lane := 0                       # 0 = center, -1 = left, 1 = right
@@ -22,6 +22,6 @@ func _physics_process(delta):
 	var target_x = current_lane * lane_offset
 	global_position.x = target_x  # Instantly warp to new lane
 
-	# Move forward constantly (Z-axis)
+	# Move forward constantly (Z-axis)adaa
 	# Apply movement
 	move_and_slide()
