12d0c87d96d0256515e14a9a488a4eef62fcfd00
Tolloviis <tuukka.heikkila07@gmail.com>
2025-04-28 10:37:53 +0300

    Hyppy toimii

--- toukka_kontrollit.gd
+++ toukka_kontrollit.gd
@@ -2,6 +2,8 @@ extends CharacterBody3D
 
 @export var lane_offset := 1.0              # Distance between lanes
 @export var total_lanes := 3                # Should be an odd number (so there's a center lane)
+@export var jump_velocity := 6.0			# Hypyn nopeus
+@export var gravity := 10.0					# Gravityn voima
 
 var current_lane := 0                       # 0 = center, -1 = left, 1 = right
 
@@ -13,6 +15,11 @@ func _physics_process(delta):
 	elif Input.is_action_just_pressed("move_right"):
 		print("Move right")
 		current_lane += 1
+	elif Input.is_action_just_pressed("move_jump"):
+		if is_on_floor():
+			print("Move jump")
+			velocity.y = jump_velocity		# Asetetaan toukan liikkuminen y-akselilla jump_velocityksi
+		
 
 	# Clamp to available lanes
 	var half = (total_lanes - 1) / 2
@@ -22,7 +29,9 @@ func _physics_process(delta):
 	var target_x = current_lane * lane_offset
 	# Smooth movement to lane
 	global_position.x = lerp(global_position.x, target_x, delta * 10.0)
+	
+	# Gravity toukalle
+	velocity.y -= gravity * delta
 
-	# Remove forward movement if not needed
 	# velocity.z = -move_speed
 	move_and_slide()
