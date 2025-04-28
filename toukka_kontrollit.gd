77857edfc37ae0f22d996302ca199d690212fb18
Elia <elia.karjalainen@student.kpedu.fi>
2025-04-28 14:32:45 +0300

    Hahmo lisätty

--- toukka_kontrollit.gd
+++ toukka_kontrollit.gd
@@ -1,4 +1,5 @@
 extends CharacterBody3D
+var animation_player : AnimationPlayer
 
 @export var lane_offset := 1.0              # Distance between lanes
 @export var total_lanes := 3                # Should be an odd number (so there's a center lane)
@@ -7,18 +8,28 @@ extends CharacterBody3D
 
 var current_lane := 0                       # 0 = center, -1 = left, 1 = right
 
+func _ready():
+	animation_player = $AnimationPlayer
+
 func _physics_process(delta):
+	if is_on_floor() and !animation_player.current_animation == "mantis_walk":
+		animation_player.speed_scale = 2.0
+		animation_player.play("mantis_run")
 	# Handle input (one press per frame)
 	if Input.is_action_just_pressed("move_left"):
 		print("Move left")
+		animation_player.play("mantis_walk")
 		current_lane -= 1
 	elif Input.is_action_just_pressed("move_right"):
 		print("Move right")
+		animation_player.play("mantis_walk")
 		current_lane += 1
 	elif Input.is_action_just_pressed("move_jump"):
 		if is_on_floor():
 			print("Move jump")
 			velocity.y = jump_velocity		# Asetetaan toukan liikkuminen y-akselilla jump_velocityksi
+			animation_player.speed_scale = 2.0
+			animation_player.play("mantis_death")
 		
 
 	# Clamp to available lanes
