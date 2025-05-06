1a7ccbc7cc54d9321908da88a228e862fdce4e61
ilari <ilari.ketopaikka@gmail.com>
2025-05-06 11:20:54 +0300

    Päihtynyt effekti

---
+++ global.gd
@@ -0,0 +1,4 @@
+
+extends Node
+
+var paihtynyt : bool = true # Päihtynyt tila
--- toukka_kontrollit.gd
+++ toukka_kontrollit.gd
@@ -3,36 +3,41 @@ var animation_player : AnimationPlayer
 
 @export var lane_offset := 1.0              # Distance between lanes
 @export var total_lanes := 3                # Should be an odd number (so there's a center lane)
-@export var jump_velocity := 4.5			# Hypyn nopeus
-@export var gravity := 10.0					# Gravityn voima
-
+@export var jump_velocity := 4.5            # Jump speed
+@export var gravity := 10.0                 # Gravity force
 var current_lane := 0                       # 0 = center, -1 = left, 1 = right
+
 
 func _ready():
 	animation_player = $AnimationPlayer
 
 func _physics_process(delta):
-	if is_on_floor() and !animation_player.current_animation == "mantis_walk":
+	if is_on_floor() and animation_player.current_animation != "mantis_walk":
 		animation_player.speed_scale = 2.0
 		animation_player.play("mantis_run")
 	# Handle input (one press per frame)
 	if Input.is_action_just_pressed("move_left"):
 		print("Move left")
-		if !animation_player.current_animation == "mantis_death":
+		if animation_player.current_animation != "mantis_death":
 			animation_player.play("mantis_walk")
-		current_lane -= 1
+		if Global.paihtynyt:
+			current_lane += 1
+		else:
+			current_lane -= 1
 	elif Input.is_action_just_pressed("move_right"):
 		print("Move right")
-		if !animation_player.current_animation == "mantis_death":
+		if animation_player.current_animation != "mantis_death":
 			animation_player.play("mantis_walk")
-		current_lane += 1
+		if Global.paihtynyt:
+			current_lane -= 1
+		else:
+			current_lane += 1
 	elif Input.is_action_just_pressed("move_jump"):
 		if is_on_floor():
 			print("Move jump")
-			velocity.y = jump_velocity		# Asetetaan toukan liikkuminen y-akselilla jump_velocityksi
+			velocity.y = jump_velocity		# Set jump velocity
 			animation_player.speed_scale = 2.0
 			animation_player.play("mantis_death")
-		
 
 	# Clamp to available lanes
 	var half = (total_lanes - 1) / 2
@@ -42,9 +47,9 @@ func _physics_process(delta):
 	var target_x = current_lane * lane_offset
 	# Smooth movement to lane
 	global_position.x = lerp(global_position.x, target_x, delta * 10.0)
-	
-	# Gravity toukalle
+
+	# Apply gravity
 	velocity.y -= gravity * delta
 
-	# velocity.z = -move_speed
+	# Move the character
 	move_and_slide()
